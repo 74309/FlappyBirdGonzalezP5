b30c81c483ae19e49c26e2c2755b2933e345bbd5
74309 <743095@student.dusd.net>
2023-12-14 13:51:21 -0800

    I need to fix score audio
    Score audio needs to be moved to Game controller

--- FlappyBirdRecreationP5Gonzalez/Assets/Scripts/Column.cs
+++ FlappyBirdRecreationP5Gonzalez/Assets/Scripts/Column.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class Column : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D other)
+    AudioSource audioSource;
+    public AudioClip Score;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+        private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.GetComponent<Bird> () != null)
         {
             GameControl.Instance.BirdScored();
+            PlaySound(Score);
         }
     }
+
+    public void PlaySound(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
+    }
 }
+
